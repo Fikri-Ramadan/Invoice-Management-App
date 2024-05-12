cd847acdedcccebfb208c30ee705866c8a90fe79
Fikri Ramadan <fikrinr.tech@gmail.com>
2024-05-12 09:22:48 +0700

    chore: init shadcn

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -1,3 +1,7 @@
+import { Button } from "./ui/button";
+
 export const Header = () => {
-  return <div>Header</div>;
+  return <div className="text-blue-500">
+    <Button variant={"secondary"}>asd</Button>
+    Header</div>;
 };
