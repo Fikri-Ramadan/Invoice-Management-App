94f8a9f9caef451060fb93c79437712249d6a7c7
Fikri Ramadan <fikrinr.tech@gmail.com>
2024-05-21 17:37:54 +0700

    feat: express validator

--- apps/api/src/routers/client.router.ts
+++ apps/api/src/routers/client.router.ts
@@ -1,4 +1,5 @@
 import { ClientController } from "@/controllers/client.controller";
+import { newClientValidator } from "@/middlewares/validator";
 import { verifyToken } from "@/middlewares/verifyJwt";
 import { Router } from "express";
 
@@ -15,7 +16,7 @@ export class ClientRouter {
   private initializeRoutes() {
     this.router.get('/', verifyToken, this.clientController.getMyClient);
     this.router.get('/:id', verifyToken, this.clientController.getClientById);
-    this.router.post('/', verifyToken, this.clientController.createClient);
+    this.router.post('/', verifyToken, newClientValidator, this.clientController.createClient);
     this.router.put('/:id', verifyToken, this.clientController.updateClient);
     this.router.delete('/:id', verifyToken, this.clientController.softDeleteClient);
   }
